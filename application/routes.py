1d74d0843d70c31431a8ebb23fe7adbd5d23fb07
Misael Casarez <mcasarez2@ucmerced.edu>
2022-11-06 21:58:21 -0800

    Modified layout

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,8 @@
 from application import app
+from flask import render_template
 
 @app.route("/")
 @app.route("/index")
+@app.route("/home")
 def index():
-    return "<h1>Hello Earth!!</h1>"
+    return render_template("home.html")
